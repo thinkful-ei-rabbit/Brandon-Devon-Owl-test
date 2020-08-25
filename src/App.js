cfee60f80b54a6b78a09ed4b53c86ec40526cd4c
Devon Reihl <devon.reihl@gmail.com>
2020-08-25 11:44:03 -0700

    side bar built

--- src/App.js
+++ src/App.js
@@ -2,9 +2,26 @@ import React from 'react';
 
 function App() {
   return (
-    <main className='App'>
-      {/* content goes here */}
-    </main>
+    <div>
+      <div className="side-bar">
+        <div className='side-bar-tools'>
+          <button>Chat</button>
+          <button>Participant</button>
+          <button>Settings</button>
+          <button>Tools</button>
+          <button>FAQ</button>
+        </div>
+        <div className='side-bar-partcipants'>
+          <div className='side-bar-participant'>
+            <img src="" alt=""/>
+            <div>
+              <div>NAME</div>
+              <div>ON STAGE</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
---
+++ src/side-bar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function sideBar() {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default sideBar
